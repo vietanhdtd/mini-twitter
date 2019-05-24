d06e0f9ddc1db20a58265f1d0899a4c07b5d3943
VY <huynh.h.vy@gmail.com>
2019-05-24 17:57:51 +0700

    add delete anchor tag

--- script.js
+++ script.js
@@ -7,34 +7,42 @@ const inputTweet = document.getElementById('inputTweet')
 
 const addNewTweet = () => {
     const newTweet = {
-        isLiked : false,
-        text : inputTweet.value,
+        isLiked: false,
+        text: inputTweet.value,
         formerTweeted: false
     }
     tweetFeed.push(newTweet)
 }
 
-console.log(tweetFeed)
 const updateTweetFeed = () => {
-    let html = '', textnode, node
-    tweetFeed.map(({isLiked, text},i) => {
-    textnode = `<li>${text}<a href="#" onclick="reTweet(${i})">Retweet</a>
-    <a href="#" onclick="toggleLike(${i})">${tweetFeed[i].isLiked ? "unlike" : "like"}</a>
+    let html = '',
+        textnode, node
+    tweetFeed.map(({
+        isLiked,
+        text
+    }, i) => {
+        textnode = `<li>${text}<a href="#" onclick="reTweet(${i})">Retweet</a>
+    <a href="#" onclick="toggleLike(${i})">${tweetFeed[i].isLiked ? "unlike" : "like"}</a><a href="#" onclick="removeItem(${i})">Remove</a>
     </li>`
-    node = html += textnode
-    document.getElementById('tweetFeed').innerHTML = node
-    inputTweet.value = ''
+        node = html += textnode
+        document.getElementById('tweetFeed').innerHTML = node
+        inputTweet.value = ''
     })
 }
 
 const reTweet = i => {
-    let retweetItem = tweetFeed.splice(i+1,0,tweetFeed[i])
+    tweetFeed.splice(i + 1, 0, tweetFeed[i])
     updateTweetFeed()
 }
 
-const toggleLike = (i) => {
+const toggleLike = i => {
     tweetFeed[i].isLiked = !tweetFeed[i].isLiked;
     updateTweetFeed()
+}
+
+const removeItem = i => {
+    tweetFeed.splice(i, 1);
+    updateTweetFeed();
 }
 
 const displayRemainingCharacter = () => {
@@ -70,8 +78,7 @@ const renderTweetFeed = () => {
 
 document.getElementById('inputTweet').addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
-      event.preventDefault();
-      document.getElementById('tweetBtn').click();
+        event.preventDefault();
+        document.getElementById('tweetBtn').click();
     }
-  })
-  
+})
